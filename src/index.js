c53338e7c777350c9be84747aedcc1cc8006a119
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2023-11-19 15:36:00 -0700

    Cats homework

---
+++ src/index.js
@@ -0,0 +1,91 @@
+// index.js
+import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+
+const breedSelect = document.querySelector(".breed-select");
+const loader = document.querySelector(".loader");
+const error = document.querySelector(".error");
+const catInfo = document.querySelector(".cat-info");
+
+const populateBreedsSelect = async () => {
+  try {
+    const breeds = await fetchBreeds();
+    breeds.forEach((breed) => {
+      const option = document.createElement("option");
+      option.value = breed.id;
+      option.text = breed.name;
+      breedSelect.add(option);
+    });
+  } catch (error) {
+    showError();
+  } finally {
+    hideLoader();
+  }
+};
+
+const showCatInfo = async (breedId) => {
+  try {
+    const catData = await fetchCatByBreed(breedId);
+    const cat = catData[0];
+    const catInfoHTML = `
+      <div class="cat-info-container">
+        <img src="${cat.url}" alt="${cat.breeds[0].name}" class="cat-image">
+        <div class="cat-text">
+          <p><strong>Breed:</strong> ${cat.breeds[0].name}</p>
+          <p><strong>Description:</strong> ${cat.breeds[0].description}</p>
+          <p><strong>Temperament:</strong> ${cat.breeds[0].temperament}</p>
+        </div>
+      </div>
+    `;
+    catInfo.innerHTML = catInfoHTML;
+
+    setCatInfoStyles();
+  } catch (error) {
+    showError();
+  } finally {
+    hideLoader();
+  }
+};
+
+const setCatInfoStyles = () => {
+  const catInfoContainer = document.querySelector(".cat-info-container");
+  const catImage = catInfoContainer.querySelector(".cat-image");
+  const catText = catInfoContainer.querySelector(".cat-text");
+
+  breedSelect.style.marginBottom = "10px"
+  catInfoContainer.style.display = "flex";
+  catInfoContainer.style.flexWrap = "wrap";
+
+  catImage.style.maxWidth = "400px";
+  catImage.style.marginRight = "20px";
+
+  catText.style.flex = "1";
+};
+
+const showLoader = () => {
+  loader.style.display = "block";
+};
+
+const hideLoader = () => {
+  loader.style.display = "none";
+};
+
+const showError = () => {
+  error.style.display = "block";
+};
+
+const hideError = () => {
+  error.style.display = "none";
+};
+
+breedSelect.addEventListener("change", (event) => {
+  const selectedBreedId = event.target.value;
+  showLoader();
+  hideError();
+  showCatInfo(selectedBreedId);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  showLoader();
+  hideError();
+  populateBreedsSelect();
+});
